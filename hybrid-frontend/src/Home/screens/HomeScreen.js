85e0736e1e331d89bea3d597ef2fdaf49395aaf5
Ignacio <ibello@miuandes.cl>
2024-10-15 15:12:53 -0300

    Home Page 2

--- hybrid-frontend/src/Home/screens/HomeScreen.js
+++ hybrid-frontend/src/Home/screens/HomeScreen.js
@@ -5,13 +5,6 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Image source={require('/home/topo/AppMOv/proyecto-del-curso-bello-pinedo/hybrid-frontend/assets/beer-icon.png')} style={styles.logo} />
-        <Text style={styles.headerTitle}>BeerMark</Text>
-        <TouchableOpacity>
-          <Text style={styles.logoutButton}>LOG OUT</Text>
-        </TouchableOpacity>
-      </View>
 
       <ScrollView style={styles.content}>0--0-
         <View style={styles.section}>
@@ -54,21 +47,6 @@ const HomeScreen = () => {
           </View>
         </View>
       </ScrollView>
-
-      <View style={styles.footer}>
-        <TouchableOpacity style={styles.footerButton}>
-          <Icon name="home" size={24} color="#000" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.footerButton}>
-          <Icon name="calendar" size={24} color="#000" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.footerButton}>
-          <Icon name="search" size={24} color="#000" />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.footerButton}>
-          <Icon name="user" size={24} color="#000" />
-        </TouchableOpacity>
-      </View>
     </View>
   );
 };
@@ -76,9 +54,6 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#1E1E1E' },
   header: { flexDirection: 'row', justifyContent: 'space-between', padding: 15, backgroundColor: '#f5c000' },
-  logo: { width: 30, height: 30 },
-  headerTitle: { fontSize: 18, fontWeight: 'bold', color: '#000' },
-  logoutButton: { fontSize: 16, color: '#000' },
 
   content: { flex: 1, padding: 15 },
   section: { marginBottom: 20 },
@@ -94,9 +69,6 @@ const styles = StyleSheet.create({
   eventDescription: { fontSize: 14, marginLeft: 10 },
 
   trendingEvent: { flexDirection: 'row', padding: 15, backgroundColor: '#f5c000', borderRadius: 10 },
-
-  footer: { flexDirection: 'row', justifyContent: 'space-between', padding: 15, backgroundColor: '#f5c000' },
-  footerButton: { padding: 10 }
 });
 
 export default HomeScreen;
