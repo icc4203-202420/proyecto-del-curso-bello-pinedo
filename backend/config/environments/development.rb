1e37e494a2c030332219c5a562650c0f4dd4013d
Ignacio <ibello@miuandes.cl>
2024-10-18 19:42:45 -0300

    Solicitud de revisión para entrega 2.1

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
-  config.hosts << /[a-z0-9-]+\.ngrok-free\.app/
+  config.hosts << /.*\.ngrok-free\.app/
 
   # Do not eager load code on boot.
   config.eager_load = false
