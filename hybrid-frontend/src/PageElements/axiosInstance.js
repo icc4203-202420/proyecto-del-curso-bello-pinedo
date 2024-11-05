fc6cc8c03ba75e3baaa9131bbe9b7dd3113c1b5b
Mpinedog <mpinedo1@miuandes.cl>
2024-11-04 22:04:38 -0300

    todo listo

--- hybrid-frontend/src/PageElements/axiosInstance.js
+++ hybrid-frontend/src/PageElements/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {NGROK_URL} from '@env';
 
 const axiosInstance = axios.create({
-  baseURL: `https://8010-200-124-48-32.ngrok-free.app/api/v1`, 
+  baseURL: `https://50ac-190-164-206-237.ngrok-free.app/api/v1`, 
   headers: {
     'Content-Type': 'application/json',
   },
