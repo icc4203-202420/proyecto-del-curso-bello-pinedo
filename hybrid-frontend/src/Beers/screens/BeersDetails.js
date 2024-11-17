769dce9f9eab0871d244e54ecaba6802634a18bc
Mpinedog <mpinedo1@miuandes.cl>
2024-11-17 11:47:57 -0300

    detalles web sockets y beerdetails

--- hybrid-frontend/src/Beers/screens/BeersDetails.js
+++ hybrid-frontend/src/Beers/screens/BeersDetails.js
@@ -27,6 +27,7 @@ function BeerDetails() {
   const [comment, setComment] = useState('');
   const [error, setError] = useState('');
   const [currentUser, setCurrentUser] = useState(null);
+  const [successMessage, setSuccessMessage] = useState('');
   const { id } = useRoute().params;
   const navigation = useNavigation();
 
@@ -76,7 +77,6 @@ function BeerDetails() {
           (review) => review.beer_id === parseInt(id)
         );
 
-        // Añade el handle del usuario a cada review
         const reviewsWithHandle = await Promise.all(
           filteredReviews.map(async (review) => {
             try {
@@ -100,29 +100,77 @@ function BeerDetails() {
     }
   };
 
+  const handleRatingChange = (newRating) => {
+    setRating(newRating);
+  };
+
+  const handleCommentChange = (text) => {
+    setComment(text);
+  };
+
+  const handleSubmit = () => {
+    const wordCount = comment.trim().split(/\s+/).length;
+  
+    if (wordCount < 15) {
+      setError('The comment must be at least 15 words.');
+      return;
+    }
+  
+    if (!rating || rating < 1 || rating > 5) {
+      setError('The rating must be between 1 and 5.');
+      return;
+    }
+  
+    setError('');
+    dispatch({ type: 'LOADING' });
+  
+    if (currentUser) {
+      axiosInstance
+        .post(`/users/${currentUser.id}/reviews`, {
+          beer_id: id,
+          rating: rating,
+          text: comment,
+        })
+        .then(() => {
+          dispatch({
+            type: 'SUBMIT_SUCCESS',
+            payload: { rating, text: comment, user_id: currentUser.id },
+          });
+  
+          // Restablece los valores
+          setComment(''); // Vacía el campo de texto
+          setSuccessMessage('Your review was submitted successfully!');
+          setTimeout(() => setSuccessMessage(''), 3000); // Ocultar después de 3 segundos
+
+  
+          // Muestra un mensaje de éxito temporal
+          setError('');
+          dispatch({ type: 'SUCCESS', success: 'Review submitted successfully!' });
+  
+          // Recarga las reviews después de un submit exitoso
+          fetchReviews();
+  
+          setTimeout(() => {
+            dispatch({ type: 'SUCCESS', success: '' }); // Limpia el mensaje después de 3 segundos
+          }, 3000);
+        })
+        .catch(() => {
+          dispatch({
+            type: 'ERROR',
+            payload: 'Error submitting the review. Please try again.',
+          });
+        });
+    } else {
+      setError('User not found. Please log in.');
+    }
+  };  
+  
+
   const renderReview = ({ item }) => (
     <View style={styles.reviewCard}>
       <Text style={styles.reviewText}>Rating: {item.rating}</Text>
       <Text style={styles.reviewText}>Comment: {item.text}</Text>
       <Text style={styles.reviewText}>Posted by: {item.user_handle || 'Unknown'}</Text>
-    </View>
-  );
-
-  const renderHeader = () => (
-    <View style={styles.card}>
-      <Text style={styles.title}>{beer.name}</Text>
-      <Text style={styles.brewery}>
-        Brewery: {beer.breweries && beer.breweries.length > 0 ? beer.breweries[0].name : 'Unknown'}
-      </Text>
-      <Text style={styles.details}>Style: {beer.style || 'Unknown'}</Text>
-      <Text style={styles.details}>Type: {beer.type || 'Unknown'}</Text>
-      <Text style={styles.details}>Hop: {beer.hop || 'Unknown'}</Text>
-      <Text style={styles.details}>Yeast: {beer.yeast || 'Unknown'}</Text>
-      <Text style={styles.details}>Malts: {beer.malts || 'Unknown'}</Text>
-      <Text style={styles.details}>IBU: {beer.ibu || 'Unknown'}</Text>
-      <Text style={styles.details}>Alcohol: {beer.alcohol || 'Unknown'}</Text>
-      <Text style={styles.details}>Blg: {beer.blg || 'Unknown'}</Text>
-      <Text style={styles.details}>Rating: {beer.avg_rating || 'No rating yet'}</Text>
     </View>
   );
 
@@ -141,11 +189,48 @@ function BeerDetails() {
         data={state.reviews}
         renderItem={renderReview}
         keyExtractor={(item, index) => index.toString()}
-        ListHeaderComponent={renderHeader}
+        ListHeaderComponent={
+          <>
+            <View style={styles.card}>
+              <Text style={styles.title}>{beer.name}</Text>
+              <Text style={styles.brewery}>
+                Brewery: {beer.breweries && beer.breweries.length > 0 ? beer.breweries[0].name : 'Unknown'}
+              </Text>
+              <Text style={styles.details}>Style: {beer.style || 'Unknown'}</Text>
+              <Text style={styles.details}>Type: {beer.type || 'Unknown'}</Text>
+              <Text style={styles.details}>Hop: {beer.hop || 'Unknown'}</Text>
+              <Text style={styles.details}>Yeast: {beer.yeast || 'Unknown'}</Text>
+              <Text style={styles.details}>Malts: {beer.malts || 'Unknown'}</Text>
+              <Text style={styles.details}>IBU: {beer.ibu || 'Unknown'}</Text>
+              <Text style={styles.details}>Alcohol: {beer.alcohol || 'Unknown'}</Text>
+              <Text style={styles.details}>Blg: {beer.blg || 'Unknown'}</Text>
+              <Text style={styles.details}>Rating: {beer.avg_rating.toFixed(1) || 'No rating yet'}</Text>
+            </View>
+            <View style={styles.card}>
+              <Text style={styles.subtitle}>Rate this beer:</Text>
+              <Rating
+                showRating
+                startingValue={rating}
+                onFinishRating={handleRatingChange}
+                style={styles.rating}
+                ratingCount={5}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Add a comment"
+                value={comment}
+                onChangeText={handleCommentChange}
+              />
+              {error ? <Text style={styles.error}>{error}</Text> : null}
+              {successMessage ? <Text style={styles.success}>{successMessage}</Text> : null}
+              <Button title="Submit Review" color="#1E1E1E" onPress={handleSubmit} />
+            </View>
+          </>
+        }
         contentContainerStyle={styles.container}
         ListEmptyComponent={<Text style={styles.noReviews}>No reviews yet.</Text>}
       />
-      <Footer /> {/* Footer fijo */}
+      <Footer />
     </View>
   );
 }
@@ -155,13 +240,20 @@ const styles = StyleSheet.create({
   container: { padding: 20 },
   card: { backgroundColor: '#f5c000', padding: 20, borderRadius: 10, marginBottom: 20 },
   title: { fontSize: 24, fontWeight: 'bold', marginBottom: 10 },
-  brewery: { fontSize: 16, marginBottom: 5 },
   details: { fontSize: 14, marginBottom: 5 },
   reviewCard: { padding: 10, backgroundColor: '#fff', borderRadius: 5, marginBottom: 10 },
   reviewText: { fontSize: 14, marginBottom: 5 },
   loadingContainer: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#1E1E1E' },
   loadingText: { marginTop: 10, color: '#f5c000' },
   noReviews: { color: '#f5c000', textAlign: 'center', marginTop: 20 },
+  input: { backgroundColor: '#fff', padding: 10, borderRadius: 5, marginBottom: 10, borderColor: '#ccc',
+    borderWidth: 1, color: '#000', fontSize: 16, },
+  rating: {marginVertical: 20, alignSelf: 'center', backgroundColor: '#1E1E1E', 
+    borderRadius: 10, padding: 10, width: '90%', },
+  success: {color: 'green', marginBottom: 10, textAlign: 'center', fontWeight: 'bold',},
+  
+    
+  
 });
 
 export default BeerDetails;
